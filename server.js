d259e2e8f4f43e157c22ea78df0e75fe3fcd7666
Mahdi Gholipour <mahdigholipourprogramming@gmail.com>
2025-09-01 10:47:03 +0000

    formatting

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import http from 'http';
-import app from './src/app.js';
-import { configDotenv } from 'dotenv';
+import http from "http";
+import app from "./src/app.js";
+import { configDotenv } from "dotenv";
 configDotenv();
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT,()=>{console.log("server run on port "+process.env.PORT);})
-
+server.listen(process.env.PORT, () => {
+  console.log("server run on port " + process.env.PORT);
+});
