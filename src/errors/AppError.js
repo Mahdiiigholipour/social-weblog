3155ea898303c2e397b824f130a2353000c2fb03
Mahdi Gholipour <mahdigholipourprogramming@gmail.com>
2025-09-05 12:07:10 +0000

    created AppError class

---
+++ src/errors/AppError.js
@@ -0,0 +1,24 @@
+import Config from "../configs/index.js";
+export class AppError extends Error {
+  constructor(message, statusCode, details = null) {
+    super(message);
+    this.name = this.constructor.name;
+    this.statusCode = statusCode;
+    this.details = details;
+    this.isOperational = true;
+    this.timestamp = new Date().toISOString();
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+
+  toJson() {
+    return {
+      name: this.name,
+      message: this.message,
+      statusCode: this.statusCode,
+      details: this.details,
+      timestamp: this.timestamp,
+      ...(Config.app.isDevelopment && { stack: this.stack }),
+    };
+  }
+}
