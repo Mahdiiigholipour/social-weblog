4c4003fb4d0c2c80cd3f1c6aa9e678d431cc405e
Mahdi Gholipour <mahdigholipourprogramming@gmail.com>
2025-09-06 15:24:59 +0000

    created NotFoundError system

---
+++ src/errors/errorTypes/notFoundError.js
@@ -0,0 +1,22 @@
+import { HTTP_STATUS } from "../../utils/constants.js";
+import { ApiError } from "../ApiError.js";
+
+export class NotFoundError extends ApiError {
+  constructor(resourse = "Resource", details = null) {
+    super(
+      `${resourse} not found`,
+      HTTP_STATUS.NOT_FOUND,
+      details,
+      "NOT_FOUND_ERROR"
+    );
+    this.name = "NotFoundError";
+  }
+
+  static user(userId) {
+    return new NotFoundError("User", { userId });
+  }
+
+  static resource(resourceName, resourceId) {
+    return new NotFoundError(resourceName, { resourceId });
+  }
+}
