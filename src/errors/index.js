214cef6ff97a938aaa88a491a24c8f563fb9e6c8
Mahdi Gholipour <mahdigholipourprogramming@gmail.com>
2025-09-07 05:47:54 +0000

    export all tools of error handling system

---
+++ src/errors/index.js
@@ -0,0 +1,8 @@
+export { AppError } from "./AppError.js";
+export { ApiError } from "./ApiError.js";
+export { ErrorHandler } from "./ErrorHandler.js";
+export { ValidationError } from "./errorTypes/ValidationError.js";
+export { AuthenticationError } from "./errorTypes/AuthenticationError.js";
+export { AuthorizationError } from "./errorTypes/AuthorizationError.js";
+export { DatabaseError } from "./errorTypes/DatabaseError.js";
+export { NotFoundError } from "./errorTypes/NotFoundError.js";
