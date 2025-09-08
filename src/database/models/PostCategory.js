4ad2808a8af40bea59a132693e51f5daf2c4c4d1
Mahdi Gholipour <mahdigholipourprogramming@gmail.com>
2025-09-08 11:11:48 +0000

    created post category fields

---
+++ src/database/models/PostCategory.js
@@ -0,0 +1,32 @@
+import { DataTypes } from "sequelize";
+import BaseModel from "./BaseModel";
+
+export default class PostCategory extends BaseModel {
+  static init(sequelize) {
+    return super.init(
+      {
+        postId: {
+          type: DataTypes.UUID,
+          primaryKey: true,
+          references: {
+            model: "Post",
+            key: "id",
+          },
+          onDelete: "CASCADE",
+        },
+        categoryId: {
+          type: DataTypes.UUID,
+          primaryKey: true,
+          references: { model: "Category", key: "id" },
+          onDelete: "CASCADE",
+        },
+      },
+      {
+        sequelize,
+        modelName: "PostCategory",
+        tableName: "post_category",
+        timestamps: true,
+      }
+    );
+  }
+}
