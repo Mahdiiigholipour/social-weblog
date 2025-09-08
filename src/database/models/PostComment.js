9501db4b247098526c1e21f090a70b4196637ff9
Mahdi Gholipour <mahdigholipourprogramming@gmail.com>
2025-09-08 11:32:56 +0000

    created post comment fields

---
+++ src/database/models/PostComment.js
@@ -0,0 +1,39 @@
+import { DataTypes } from "sequelize";
+import BaseModel from "./BaseModel";
+
+export default class PostComment extends BaseModel {
+  static init(sequelize) {
+    return super.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          defaultValue: DataTypes.UUIDV4,
+          primaryKey: true,
+        },
+        postId: {
+          type: DataTypes.UUID,
+          references: {
+            model: "Post",
+            key: "id",
+          },
+          onDelete: "CASCADE",
+        },
+        userId: {
+          type: DataTypes.UUID,
+          references: {
+            model: "User",
+            key: "id",
+          },
+          onDelete: "SET NULL",
+        },
+        content: { type: DataTypes.TEXT, allowNull: false },
+      },
+      {
+        sequelize,
+        modelName: "PostComment",
+        tableName: "post_comment",
+        timestamps: true,
+      }
+    );
+  }
+}
---
+++ src/database/models/PostLike.js
@@ -0,0 +1,35 @@
+import { DataTypes } from "sequelize";
+import BaseModel from "./BaseModel";
+
+export default class PostLike extends BaseModel {
+  static init(sequelize) {
+    return super.init(
+      {
+        postId: {
+          type: DataTypes.UUID,
+          primaryKey: true,
+          references: {
+            model: "Post",
+            key: "id",
+          },
+          onDelete: "CASCADE",
+        },
+        userId: {
+          type: DataTypes.UUID,
+          primaryKey: true,
+          references: {
+            model: "User",
+            key: "id",
+          },
+          onDelete: "CASCADE",
+        },
+      },
+      {
+        sequelize,
+        modelName: "PostLike",
+        tableName: "post_like",
+        timestamps: true,
+      }
+    );
+  }
+}
