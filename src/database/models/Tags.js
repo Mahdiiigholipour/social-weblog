134e8274a7211f5c1a1f82dfae2502e4dc0a9309
Mahdi Gholipour <mahdigholipourprogramming@gmail.com>
2025-09-08 11:17:20 +0000

    created tag fields

---
+++ src/database/models/Tags.js
@@ -0,0 +1,24 @@
+import { DataTypes } from "sequelize";
+import BaseModel from "./BaseModel";
+
+export default class Tag extends BaseModel {
+  static init(sequelize) {
+    return super.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          defaultValue: DataTypes.UUIDV4,
+          primaryKey: true,
+        },
+        name: { type: DataTypes.STRING(50), unique: true, allowNull: false },
+        slug: { type: DataTypes.STRING(50), unique: true, allowNull: false },
+      },
+      {
+        sequelize,
+        modelName: "Tag",
+        tableName: "tag",
+        timestamps: false,
+      }
+    );
+  }
+}
