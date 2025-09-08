b919dde6d96a9b9ba1896f879a80a7921038f9b0
Mahdi Gholipour <mahdigholipourprogramming@gmail.com>
2025-09-08 11:42:01 +0000

    created bookmark fields

---
+++ src/database/models/Bookmark.js
@@ -0,0 +1,32 @@
+import { DataTypes } from "sequelize";
+import BaseModel from "./BaseModel";
+
+export default class Bookmark extends BaseModel {
+  static init(sequelize) {
+    return super.init(
+      {
+        userId: {
+          type: DataTypes.UUID,
+          primaryKey: true,
+          references: {
+            model: "User",
+            key: "id",
+          },
+          onDelete: "SET NULL",
+        },
+        postId: {
+          type: DataTypes.UUID,
+          primaryKey: true,
+          references: { model: "Post", key: "id" },
+          onDelete: "CASCADE",
+        },
+      },
+      {
+        sequelize,
+        modelName: "Bookmark",
+        tableName: "bookmark",
+        timestamps: true,
+      }
+    );
+  }
+}
