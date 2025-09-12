467c4dad970daf177bfb386b31455cdea6c76eac
Mahdi Gholipour <mahdigholipourprogramming@gmail.com>
2025-09-12 19:15:55 +0000

    developed authenticate middleware

---
+++ src/middlewares/authenticate.js
@@ -0,0 +1,19 @@
+import { AuthenticationError } from "../errors";
+import Token from "../utils/tokenService";
+
+export default authenticate = async (req, res, next) => {
+  const tokenService = new Token();
+  try {
+    const token =
+      req.cookies.accessToken || req.headers.authorization?.split(" ")[1];
+
+    if (!token) throw AuthenticationError.noToken();
+
+    const payload = tokenService.verifyAccessToken(token);
+
+    req.user = { id: payload.sub, role: payload.role };
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
